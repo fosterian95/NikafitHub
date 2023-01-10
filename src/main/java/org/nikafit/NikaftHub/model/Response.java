4f9f7a93964a62956651871c7e67f45894658c0a
Ian Foster <fosterian95@gmail.com>
2023-01-10 10:09:36 +0000

    10 Jan 23 - Added local datasource and Http Response model

---
+++ src/main/java/org/nikafit/NikaftHub/model/Response.java
@@ -0,0 +1,22 @@
+package org.nikafit.NikaftHub.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL)
+public class Response {
+    protected LocalDateTime timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected String message;
+    protected String developerMessage;
+    protected Map<?, ?> data;
+}
