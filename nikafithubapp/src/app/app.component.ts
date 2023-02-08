40e51d65bcd176bda08bdce93d3ae08679f5cd33
Ian Foster <fosterian95@gmail.com>
2023-02-08 15:36:10 +0000

    08 Feb 23 - Emptied template html file and updated app component file

--- nikafithubapp/src/app/app.component.ts
+++ nikafithubapp/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'nikafithubapp';
+export class AppComponent implements OnInit{
+  appState$: Observable<AppState<CustomResponse>> | undefined;
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.userService.users$
+    .pipe(
+      map(response => {
+        return { dataState: DataState.LOADED_STATE, appData: response }
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error: error })
+      })
+    )
+  }
 }
